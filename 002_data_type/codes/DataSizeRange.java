a88f0f7cb89c53e3e00493ce8e1d5597f3536e31
Alphanumeric Str <mint.colorfuls@gmail.com>
2025-12-26 15:14:14 +0700

    Create DataSizeRange.java

---
+++ 002_data_type/codes/DataSizeRange.java
@@ -0,0 +1,9 @@
+public class DataSizeRange {
+    public static void main(String[] args) {
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
